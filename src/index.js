3e0c3d07c97a2461a18f4a13390d711aa693119c
Sodiq Olaiya <sodiq.olaiya@centricgateway.com>
2021-10-03 18:20:08 +0100

    completed user management app

--- src/index.js
+++ src/index.js
@@ -30,11 +30,7 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <PersistGate
-          loading={
-            <div>Loading...</div>
-          }
-          persistor={persistor}>
+        <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
           <App />
         </PersistGate>
       </Provider>,
